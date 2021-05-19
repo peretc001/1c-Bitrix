33169d4cddb132eb4ee8a35140829cc106035c68
Igor Krasovsky <peretc001@mail.ru>
2021-05-19 22:49:31 +0300

    module email

---
+++ module_krasovsky.email/install/admin/email_list.php
@@ -0,0 +1,3 @@
+<?
+require_once($_SERVER["DOCUMENT_ROOT"]."/local/modules/krasovsky.email/admin/email_list.php");
+?>
---
+++ module_krasovsky.email/install/version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+	"VERSION" => "1.0.0",
+	"VERSION_DATE" => "2020-08-13 20:00:00"
+);
+?>
